571b9b8ad348a021dde53de6ea6aa93bc7fbd6f5
mtmarctoni <marctonimas@protonmail.com>
2025-02-26 17:25:06 +0100

    Exeercise 7

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const { Todo } = require('../mongo')
 const router = express.Router();
+const {MONGO_URL} = require('../util/config')
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
-  const todos = await Todo.find({})
-  res.send(todos);
+  try {
+    console.log(MONGO_URL);
+    
+    const todos = await Todo.find({})
+    res.send(todos);
+  } catch (error) {
+    console.error(error)
+    res.status(500).send({ message: 'Error fetching todos' })
+    }
 });
 
 /* POST todo to listing. */
@@ -21,11 +29,20 @@ const singleRouter = express.Router();
 
 const findByIdMiddleware = async (req, res, next) => {
   const { id } = req.params
+  
   req.todo = await Todo.findById(id)
   if (!req.todo) return res.sendStatus(404)
 
   next()
 }
+
+const findByIdAndUpdateMiddleware = async (req, res, next) => {
+  const id = req.todo._id
+  req.todo = await Todo.findByIdAndUpdate(id, req.body, { new: true })
+  if (!req.todo) return res.sendStatus(404)
+  next()
+}
+  
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
@@ -35,12 +52,23 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const todo = req.todo
+
+  if(todo){
+    return res.json(todo)
+  }
+   
+  res.sendStatus(405);
 });
 
 /* PUT todo. */
-singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+singleRouter.put('/', findByIdAndUpdateMiddleware, async (req, res) => {
+  const todo = req.todo
+  if (todo) {
+    return res.json(todo)
+  }
+  res.sendStatus(405);
+   
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
